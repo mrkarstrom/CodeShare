64e87bdbd5c166a30183d6299d03369b515cd1c8
Magnus Karström <magnus@yesweb.se>
2023-05-15 10:11:38 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container mx-auto flex h-full items-center justify-center">
+	<div class="grid min-w-full grid-cols-1 gap-4 md:min-w-[750px]">
+		<h3 class="py-6 text-center">Create a Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y">
+			<label for="" class="label"></label>
+		</div>
+	</div>
+</div>
