3822e02d60b248c5a6d7c0b569fd09d4b5bc455e
Magnus Karström <magnus@yesweb.se>
2023-05-16 00:33:21 +0200

    styling the code snippet card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,59 @@
 <script lang="ts">
-import {
-    snippetStore,
-    addSnippet
-} from '../stores/SnippetStore';
-import CodeSnippetCard from '$lib/components/CodeSnippetCard.svelte';
-import type {
-    PageData
-} from './$types';
+	import { snippetStore, addSnippet } from '../stores/SnippetStore';
+	import CodeSnippetCard from '$lib/components/CodeSnippetCard.svelte';
+	import type { PageData } from './$types';
 
-export let data: PageData;
+	export let data: PageData;
 
-let formData: CodeSnippetInput = {
-    title: '',
-    language: 'html',
-    code: ''
-};
+	let formData: CodeSnippetInput = {
+		title: '',
+		language: 'html',
+		code: ''
+	};
 
-snippetStore.set(data.snippets);
+	snippetStore.set(data.snippets);
 </script>
 
 <div class="container mx-auto flex h-full items-center justify-center">
-    <div class="grid min-w-full grid-cols-1 gap-4 md:min-w-[750px]">
-        <h3 class="py-6 text-center">Create a Code Snippet</h3>
-        <div class="space-y card w-full p-4 text-token">
-            <label class="label" for="title">
-                <span>Snippet Title</span>
-                <input
-                    type="text"
-                    class="input"
-                    placeholder="Input title here..."
-                    name="title"
-                    bind:value={formData.title}
-                    />
-            </label>
-            <label class="label" for="selection">
-                <span>Programming Language</span>
-                <select class="select" name="selection" bind:value={formData.language}>
-                    <option value="html">html</option>
-                    <option value="css">CSS</option>
-                    <option value="typescript">Typescript</option>
-                </select>
-            </label>
-            <label class="label" for="codesnippet">
-                <span>Code Snippet</span>
-                <textarea
-                    class="textarea"
-                    rows="4"
-                    placeholder="Enter your code snippet here..."
-                    bind:value={formData.code}
-                    />
-                    </label>
-                    <button class="btn btn-sm variant-filled-primary" on:click={()=> addSnippet(formData)}>
-                        Create Snippet
-                    </button>
-                    </div>
-                    <div class="py-6 text-center">
-                        <h2>My Code Snippets</h2>
-                    </div>
-                    {#each $snippetStore as snippet, index}
-                    <CodeSnippetCard snippet={snippet} index={index}/>
-                        {/each}
-                        </div>
-                        </div>
+	<div class="grid min-w-full grid-cols-1 gap-4 md:min-w-[750px]">
+		<h3 class="py-6 text-center">Create a Code Snippet</h3>
+		<div class="space-y card rounded-md w-full p-4 text-token">
+			<label class="label" for="title">
+				<span>Snippet Title</span>
+				<input
+					type="text"
+					class="input rounded-lg"
+					placeholder="Input title here..."
+					name="title"
+					bind:value={formData.title}
+				/>
+			</label>
+			<label class="label" for="selection">
+				<span>Programming Language</span>
+				<select class="select rounded-lg" name="selection" bind:value={formData.language}>
+					<option value="html">html</option>
+					<option value="css">CSS</option>
+					<option value="typescript">Typescript</option>
+				</select>
+			</label>
+			<label class="label" for="codesnippet">
+				<span>Code Snippet</span>
+				<textarea
+					class="textarea rounded-lg"
+					rows="4"
+					placeholder="Enter your code snippet here..."
+					bind:value={formData.code}
+				/>
+			</label>
+			<button class="btn btn-sm variant-filled-primary" on:click={() => addSnippet(formData)}>
+				Create Snippet
+			</button>
+		</div>
+		<div class="py-6 text-center">
+			<h2>My Code Snippets</h2>
+		</div>
+		{#each $snippetStore as snippet, index}
+			<CodeSnippetCard {snippet} {index} />
+		{/each}
+	</div>
+</div>
